0a80cda00ea7672db0d5ce663da5d5a893260615
Chris Hicks <chicks@gilt.com>
2012-06-16 16:42:37 -0400

    some kind of tumblr functionality

--- web.js
+++ web.js
@@ -1,15 +1,44 @@
-var express = require('express');
+var express = require('express'),
+  Tumblr = require(__dirname + '/lib/tumblr.js');
+
 
 var app = express.createServer(express.logger());
+
+app.configure(function () {
+  /*
+      Since we're going to have such a variety of things, it doesn't make sense to
+      enforce a layout. If you would like to use our very basic one, you may set
+      layout : true when you define your route
+  */
+  app.set('view options', { layout: false});
+
+  // allow routes to override static pages among other things
+  app.use(app.router);
+
+  // everything else in the public folder
+  app.use(express.static(__dirname + '/public'));
+
+  // views in the views folder
+  //app.set('views', __dirname + '/views');
+});
+
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.get('/showProductForm', function (request, response) {
+  response.send("The Product Form!");
+});
+
+app.get('/oauth', function (request, response) {
+  Tumblr.authUrl(function (url) {
+    response.redirect(url);
+  });
+});
+
+
 var port = process.env.PORT || 5000;
-
-
-
 
 console.log(" // ");
 console.log(" // ");
